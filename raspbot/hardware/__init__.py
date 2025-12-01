9d5264d2f53813ba1bd49cf2047a7a967a51800c
jiharangrang <tanrang@gachon.ac.kr>
2025-12-01 14:00:50 +0900

    불러오기 문제2

--- raspbot/hardware/__init__.py
+++ raspbot/hardware/__init__.py
@@ -1,6 +1,6 @@
 """하드웨어 추상화 계층(HAL) 패키지."""
 
 from .raspbot import RaspbotHardware, MockRaspbot
-from .camera import Camera
+from .camera import Camera, CameraConfig
 
-__all__ = ["RaspbotHardware", "MockRaspbot", "Camera"]
+__all__ = ["RaspbotHardware", "MockRaspbot", "Camera", "CameraConfig"]
